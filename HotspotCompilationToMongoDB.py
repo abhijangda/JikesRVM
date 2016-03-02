5f619c895c6cc3088ec8861553e0091ea7281b45
abhijangda <abhijangda@hotmail.com>
2016-03-02 12:41:16 +0000

    Update optimization level instead of inserting again

--- HotspotCompilationToMongoDB.py
+++ HotspotCompilationToMongoDB.py
@@ -24,10 +24,25 @@ for d in re.findall(r'\s*(\d+)\s*(\d+)\s*([%s!bn]*)\s*(\d)\s*([\w\.\:\:]+)', s):
     #d[2] contains compilation flags
     #d[3] is the teired compilation level
     #d[4] is the method descriptor
+    cursor = collection.find ({"methodFullDesc":d[4].strip()})
+    doc = None
+    for document in cursor:
+        doc = document
+        break
+    desc = d[4].strip ()
+    desc = '/'+ desc.replace('::', '.').replace('.', '/')
 
-    post = {"optLevel":int(d[3].strip()),
-            "methodFullDesc":d[4].strip ()}
-    collection.insert_one(post)
-
+    if (doc == None):
+        post = {"optLevel":int(d[3].strip()),
+                "methodFullDesc":desc}
+        collection.insert_one(post)
+    elif doc["optLevel"] < int(d[3].strip()):
+        collection.update_one({"methodFullDesc": desc},
+                              {
+                                  "$set": {
+                                      "optLevel": int(d[3].strip())
+                                      }
+                                  })
+        
 client.close()
     
