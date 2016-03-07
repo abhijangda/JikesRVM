ce4de23c8c24982948cddb8950eb0c17ee54b538
Kristian Hentschel <Kristian.Hentschel@glasgow.ac.uk>
2016-03-07 14:42:55 +0000

    Add taskset functionality for cpu affinity, and add script 3_1 to make use of this.
    Also updated 3 to use bulkcompile flag.

--- experiments/3_1_large.py
+++ experiments/3_1_large.py
@@ -3,20 +3,22 @@ import common
 # This experiment runs the same benchmark twice, to observe the behavior with an empty external database,
 # and subsequently with a populated database.
 
-benchmarks = ['avrora', 'lusearch', 'jython', 'luindex', 'xalan', 'pmd', 'sunflow']
+benchmarks = ['avrora', 'jython', 'sunflow']
 git_checkout = 'BulkRead'
-prefix = '3'
-timelimit = 60
+prefix = '3_1'
+timelimit = 180
+taskset = '0-7'
+dacapo_args = ['-n', '2', '-t', '8', '-s', 'large']
 
 try:
     common.drop_mongo_collection('AOSDatabase', 'AOSCollection')
-    common.init('3', git_checkout, timelimit=timelimit)
+    common.init('3', git_checkout, timelimit=timelimit, taskset=taskset)
 
     n = common.get_repetitions()
 
     for b in benchmarks:
         common.open_csv(b)
-        common.write_csv(['i', 'base1', 'base2', 'vm1', 'vm2'])
+        common.write_csv(['i', 'base', 'vm1', 'vm2'])
 
         for i in range(n):
             print '---', b, i+1, 'of', n, '---'
@@ -27,15 +29,14 @@ try:
             common.drop_mongo_collection('AOSDatabase', 'DCGCollection')
 
             # Baseline runs
-            for k in range(2):
-                print '--', b, 'Baseline', k + 1, '--'
-                results += [common.run_dacapo(b)]
+            print '--', b, 'Baseline', '--'
+            results += [common.run_dacapo(b, dacapo_args=dacapo_args)]
 
             # AOS database enabled runs
             print '--', b, 'AOSDB VM Writing', '--'
-            results += [common.run_dacapo(b, vm_args=['-use_aosdb1000'])]
+            results += [common.run_dacapo(b, vm_args=['-use_aosdb1000'], dacapo_args=dacapo_args)]
             print '--', b, 'AOSDB VM Reading', '--'
-            results += [common.run_dacapo(b, vm_args=['-use_aosdbbulkcompile'])]
+            results += [common.run_dacapo(b, vm_args=['-use_aosdbbulkcompile'], dacapo_args=dacapo_args)]
 
             # convert all results to strings and store them
             common.write_csv([i] + map(str, results))
--- experiments/common.py
+++ experiments/common.py
@@ -23,10 +23,12 @@ __MONGO_DRIVER_JAR__ = 'mongo-java-driver-2.11.3.jar'
 # state taken from funciton or program arguments
 __NUM_REPETITIONS__ = 3
 __TIMELIMIT__ = 0
+__TASKSET__ = None
 __PREFIX__ = 'none'
 __CSV_FILE__ = None
 __CSV_WRITER__ = None
 __LOG__ = logging
+__ARGS__ = None
 
 # set a temporary repository root if an experiment requires a specific commit checked out
 __JIKES_EXPERIMENT_TEMP_ROOT__ = ''
@@ -42,10 +44,11 @@ def reset_root(root=__JIKES_EXPERIMENT_ORIGINAL_ROOT__):
     __JIKES_EXPERIMENT_TEMP_ROOT__ = root
     __JIKES_EXPERIMENT_TEMP_BIN__ = os.path.join(root, 'dist/development_x86_64-linux/')
 
-def init(prefix, commit, timelimit=0):
-    global __NUM_REPETITIONS__, __PREFIX__, __RESULTS_DIR__, __TIMELIMIT__, __ARGS__, __LOG__
+def init(prefix, commit, timelimit=0, taskset=None):
+    global __NUM_REPETITIONS__, __PREFIX__, __RESULTS_DIR__, __TIMELIMIT__, __TASKSET__, __ARGS__, __LOG__
 
     __TIMELIMIT__ = timelimit
+    __TASKSET__ = taskset
     __ARGS__ = parse_arguments()
 
     if not __ARGS__.reuse_root:
@@ -78,14 +81,15 @@ def init(prefix, commit, timelimit=0):
     __LOG__.addHandler(handler)
 
 def parse_arguments():
-    global __NUM_REPETITIONS__, __TIMELIMIT__
+    global __NUM_REPETITIONS__, __TIMELIMIT__, __TASKSET__
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-delete', action='store_true', help='Do not delete the temporary folder after completing the experiment. Useful to manually re-run some tests.')
     parser.add_argument('--reuse-root', help='Pass the path to a previously built Jikes repository root to re-use binaries from there instead of re-building. Implies --no-delete')
     parser.add_argument('--compile-only', action='store_true', help='Build jikes and exit. Implies --no-delete.')
-    parser.add_argument('-n', help='override the number of repetitions', type=int)
+    parser.add_argument('-n', help='override the number of experiment (not dacapo!) repetitions', type=int)
     parser.add_argument('-t', help='override the time limit for each benchmark (seconds)', type=int)
+    parser.add_argument('-c', help='override the cpu list used for benchmark runs given to taskset -c')
 
     args = parser.parse_args()
 
@@ -97,6 +101,10 @@ def parse_arguments():
 
     if args.t and args.t > 0:
         __TIMELIMIT__ = args.t
+
+    if args.c:
+        __TASKSET__ = args.c
+
     return args
 
 def get_repetitions():
@@ -183,7 +191,11 @@ def run_rvm(args):
     # generate timelimit arguments if needed
     timelimit_args = []
     if __TIMELIMIT__ > 0:
-        timelimit_args = ['timelimit', '-t', str(__TIMELIMIT__), '-T', str(1)]
+        timelimit_args = ['timelimit', '-t', str(__TIMELIMIT__), '-T', '1']
+
+    taskset_args = []
+    if __TASKSET__:
+        taskset_args = ['taskset', '-c', __TASKSET__]
 
     # generate rvm arguments using absolute paths if needed
     rvm_exec = 'rvm'
@@ -197,7 +209,7 @@ def run_rvm(args):
     rvm_args = [rvm_exec, bootclasspath]
 
     # build up all arguments
-    args = timelimit_args + rvm_args + args
+    args = taskset_args + timelimit_args + rvm_args + args
 
     # print executed command:
     __LOG__.info(string.join(args))
@@ -205,16 +217,16 @@ def run_rvm(args):
     try:
         return subprocess.check_output(args, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as error:
-        print 'ERROR', 'child process exited with non-zero exit code', error.returncode
+        __LOG__.error('child process exited with non-zero exit code ' + str(error.returncode))
         return error.output
 
-def run_dacapo(benchmark, size='default', repetitions=1, vm_args=[], dacapo_args=[]):
+def run_dacapo(benchmark, vm_args=[], dacapo_args=[]):
     ''' runs the specified dacapo instance on the newly built rvm and returns
     its runtime in milliseconds. Use vm_args to specify -use_aosdb etc. '''
     dacapo_path = download_dacapo()
 
     # define the benchmark arguments, prepend any extra arguments to the vm
-    args = vm_args + ['-jar', dacapo_path, benchmark, '-s', size, '-n', str(repetitions)] + dacapo_args
+    args = vm_args + ['-jar', dacapo_path, benchmark] + dacapo_args
 
     # run the RVM with these arguments
     output = run_rvm(args)
@@ -226,9 +238,9 @@ def run_dacapo(benchmark, size='default', repetitions=1, vm_args=[], dacapo_args
         time = int(re.findall(r'PASSED in (\d+)', output)[0])
     except IndexError:
         time = -1
-        __LOG__.warn('dacapo benchmark may have failed, could not determine PASSED time, recording -1')
+        __LOG__.warn('dacapo benchmark may have failed, could not determine PASSED time.')
 
-    __LOG__.info(str(time))
+    __LOG__.info("Sum of dacapo reported times: %d" % time)
     return time
 
 def download_dacapo():
@@ -243,13 +255,13 @@ def download_dacapo():
 def drop_mongo_collection(database, collection):
     ''' uses the mongo shell to drop the specified collection from a database. '''
     args = ['mongo']
-    commands = ['use '+database, 'db.'+collection+'.drop()', 'exit']
+    commands = ['use %s' % database, 'db.%s.drop()' % collection, 'exit']
 
     p = subprocess.Popen(args, stdin=subprocess.PIPE)
     p.communicate(string.join(commands, '\n'))
     p.wait()
 
-    __LOG__.info('dropped mongo collection '+database+' : '+collection)
+    __LOG__.info('dropped mongo collection %s : %s' % (database, collection))
 
 
 # These methods, open_csv, write_csv, close_csv must be called in this order to ensure only one file is open a time.
