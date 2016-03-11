a52d75946fbf8309412d893c4b1b8a945a3aa893
Kristian Hentschel <Kristian.Hentschel@glasgow.ac.uk>
2016-03-11 16:38:12 +0000

    Add experiment script for comparing results of large runs with different number of threads.

---
+++ experiments/4_threadcount.py
@@ -0,0 +1,56 @@
+import common
+
+# This experiment runs each benchmark with different number of threads
+# to see whether our speedup scales or if we are still hindered by lock contention on the lookup queues.
+
+benchmarks = ['avrora', 'jython', 'lusearch', 'sunflow']
+git_checkout = 'BulkRead'
+prefix = '4'
+timelimit = 600
+taskset = '0-15'
+dacapo_args = ['-n', '2', '-s', 'large']
+
+dacapo_threadcount = [1, 2, 4, 8, 16]
+
+try:
+    common.drop_mongo_collection('AOSDatabase', 'AOSCollection')
+    common.init(prefix, git_checkout, timelimit=timelimit, taskset=taskset)
+
+    n = common.get_repetitions()
+
+    for b in benchmarks:
+        common.open_csv(b)
+
+        # write CSV header row
+        headers = ['i']
+        for t in dacapo_threadcount:
+            headers += ['base-%d' % t, 'aosdb-%d' % t, 'aosdboptcompile-%d' %t]
+        common.write_csv(headers)
+
+        # run benchmarks
+        for i in range(n):
+            print '---', b, i+1, 'of', n, '---'
+            results = []
+
+            for t in dacapo_threadcount:
+                # Clean MongoDB collection
+                common.drop_mongo_collection('AOSDatabase', 'AOSCollection')
+                common.drop_mongo_collection('AOSDatabase', 'DCGCollection')
+
+                da = dacapo_args + ['-t', str(t)]
+
+                # Baseline run
+                print '--', b, 'Baseline', '--'
+                results += [common.run_dacapo(b, dacapo_args=da)]
+
+                # AOS database enabled runs
+                results += [common.run_dacapo(b, vm_args=['-use_aosdb1000'], dacapo_args=da)]
+
+                results += [common.run_dacapo(b, vm_args=['-use_aosdboptcompile'], dacapo_args=da)]
+
+                # convert all results to strings and store them
+                common.write_csv([i] + map(str, results))
+
+        common.close_csv()
+finally:
+    common.teardown()
